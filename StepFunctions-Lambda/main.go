ab7ccc1427e6d35fa0d777ffff876fad5aefdf48
konan080211 <konanforbis@gmail.com>
2022-06-27 20:51:56 +0900

    fix: ファイルの作成

---
+++ StepFunctions-Lambda/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// usersテーブルからタスクの取得
+
+	// userごとに指定の数のテストデータを作成
+
+	// tweetsテーブルにインサート
+
+	// usersテーブルのアップデート
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(res),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
